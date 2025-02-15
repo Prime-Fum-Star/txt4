d83f70826d86a03117e257c5ac11c549a0f0fbbb
Prime-Fum-Star <primefuntv881@gmail.com>
2025-02-15 16:17:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6305751172:AAG3f0lLXE-ivB1r3xP0VSvUfR0nKfURYfM")
-    API_ID = int(os.environ["API_ID", 29759544]
-    API_HASH = os.environ["API_HASH", "97030a871720998c6efc5debcce67aa4"]
-    AUTH_USERS = "6952370497"""
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8177990799:AAGFyMi4DQ3Sc41GVqKt8-3XY-E2iinWuH8")
+    API_ID = int(os.environ["API_ID", 25645046]
+    API_HASH = os.environ["API_HASH", "568c366b5573c5c349f0f0f25c3a4ccd"]
+    AUTH_USERS = "6676704302"""
 
